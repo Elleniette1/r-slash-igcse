d0403b8b46a2ccbec7f58dcd1ce2d0a77b20c9e4
Elleniette1 <ellemin.f@gmail.com>
2026-01-14 23:12:53 +0000

    Finish parsing + collecting

--- paperparser.py
+++ paperparser.py
@@ -3,25 +3,22 @@ import pandas as pd
 import re
 
 def parse_threshold(paper_path: str):
-    reader = PdfReader("thresholds/IGCSE/741349-additional-mathematics-0606-june-2025-grade-threshold-table.pdf")
-
+    reader = PdfReader(paper_path)
+    if 'IGCSE' in paper_path and "9-1" not in paper_path and "additional-mathematics" not in paper_path:
+        IGCSE = 2
+    elif "9-1" in paper_path:
+        IGCSE = 3
+    else:
+        IGCSE = 0
+        
     text = ""
     # Get all pages into one big string
     for page in reader.pages:
         text += (page.extract_text())
-    text = text.replace("""Learn more! For more information please visit www.cambridgeinternational.org/alevel or contact Customer 
-    Services on +44 (0)1223 553554 or email info@cambridgeinternational.org""", "")
-    text = text.split("\nThe overall thresholds for the different grades were set as follows.  ") # This line always come before the consolidated table
-    text = (text[1]
-        .replace("Grade thresholds continued ","")
-        .replace("A* A B C D E ", "")
-        .replace("A*","\nA*")
-        .replace(", ", ",")
-        .replace("  ", " "))
+    text = text.split("\n")
 
     # Removing empty lines (parser generated them idk)
-    text = text.split("\n")
-    text = [i for i in text 
+    text = [i.strip() for i in text 
             if i != "" 
             and i != " " 
             and "Cambridge" not in i
@@ -30,8 +27,31 @@ def parse_threshold(paper_path: str):
             and "Combination" not in i
             and "components" not in i
             and "weighting" not in i
-            and "mark after" not in i]
+            and "mark after" not in i
+            and "Grade" not in i
+            and "Component" not in i
+            and "Learn" not in i
+            and "Services" not in i
+            and "A Level" not in i
+            and "refer" not in i
+            and "raw" not in i
+            and "mark" not in i
+            and "available" not in i
+            and "A B C D E" not in i
+            and "threshold" not in i
+            and "exam" not in i
+            and "A2-only" not in i
+            and "email" not in i
+            and "Core" not in i]
 
+
+    for i in range(len(text)):
+        if len(text[i]) < 7:
+            try:
+                text[i-1] = text[i-1] + " " + text[i] + " " + text[i+1]
+            except:
+                pass
+    text = [item for item in text if re.search(r'[A-Za-z]', item)]
 
     # Finding the Date
     date_extractor = reader.pages[0].extract_text()
@@ -44,28 +64,59 @@ def parse_threshold(paper_path: str):
             date_extractor = re.search(r"March \d{4}", date_extractor).group(0)
 
     # Table Headers
-    table_header = [
-        'option', 
-        'max mark', 
-        'combination',
-        'A*', 'A', 'B', 'C', 'D', 'E']
+    if IGCSE and not "9-1" in paper_path:
+        table_header = [
+            'option', 
+            'max mark', 
+            'combination',
+            'a*', 'a', 'b', 'c', 'd', 'e', 'f','g']
+    elif "9-1" in paper_path:
+        table_header = [
+            'option', 
+            'max mark', 
+            'combination',
+            '9', '8', '7', '6', '5', '4','3','2','1']
+    else:
+        table_header = [
+            'option', 
+            'max mark', 
+            'combination',
+            'A*', 'A', 'B', 'C', 'D', 'E']
     data = pd.DataFrame(columns=table_header)
     data.columns = data.columns.str.lower()
 
     j = 0
     for i in text:
-        k = i.split(" ")
-        k.remove("")
-        print(k)
-        data.loc[j] = k
+        if i[0].isdigit() or i[0].isspace():
+            text.remove(i)
+            continue
+        i = i.replace(", ", ",")
+        i = i.split(" ")
+        if "(" in str(i[1]) or ")" in str(i[1]):
+            i[0] = i[0] + " " + i[1]
+            i.pop(1)
+        if len(i) < 9+IGCSE:
+            i = i[:1] + [0] + i[1:]
+        i = [k for k in i if k != ""]
+        if len(i) > 9+IGCSE:
+            i = i[:9+IGCSE]
+        data.loc[j] = i
         j += 1
-
+        
+    data["option"] = data["option"] + " " + data["combination"]
+    data.drop(columns=["combination"], inplace=True)
     data.set_index("option", inplace=True)
     data["date"] = date_extractor
     data.replace("–", 0, inplace=True)
-    data[['max mark', 'a*', 'a', 'b', 'c', 'd', 'e']] = data[['max mark', 'a*', 'a', 'b', 'c', 'd', 'e']].astype(int)
+    try:
+        data.iloc[:, :-1] = data.iloc[:, :-1].astype(int)
+    except:
+        data.iloc[:, :-1] = data.iloc[:, :-1].astype(int)
 
-    a2_table = data.copy().loc[data["max mark"]>= 131,:]
-    as_table = data.copy().loc[data["max mark"]< 131,:].replace(0, pd.NA)
+    if IGCSE == 3:
+        a2_table = data.copy().loc[(data["max mark"] >= 131) | (data["9"] >= 0), :]
+    else: 
+        a2_table = data.copy().loc[(data["max mark"] >= 131) | (data["a*"] >= 0), :]
+        as_table = data.copy().loc[data["max mark"]< 131,:]
 
-    return data, as_table, a2_table
+    return data, 1, a2_table
